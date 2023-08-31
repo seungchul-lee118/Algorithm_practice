7b2fd0abd22ffdc140e8af17cbc0193125a08f2b
seungchul <seungchul0118@gmail.com>
2023-08-31 12:26:59 +0900

    programmers lv.2 문자열 압축

---
+++ src/main/java/bootcamp/example/lv2/StringCompress.java
@@ -0,0 +1,69 @@
+package bootcamp.example.lv2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
+public class StringCompress {
+    public static void main(String[] args) {
+        String s1 = "aabbaccc";
+        String s2 = "ababcdcdababcdcd";
+        String s3 = "abcabcdede";
+        String s4 = "abcabcabcabcdededededede";
+        String s5 = "xababcdcdababcdcd";
+
+        System.out.println(solution(s1));
+        System.out.println(solution(s2));
+        System.out.println(solution(s3));
+        System.out.println(solution(s4));
+        System.out.println(solution(s5));
+    }
+
+    // 프로그래머스 lv.2 문자열 압축
+
+    public static int solution(String s) {
+        int answer = s.length();
+        int splitUnit = s.length() / 2;
+
+        while (splitUnit > 0) {
+            Stack<String> units = new Stack<>();
+            int maxUnitCount = s.length() / splitUnit;
+            for (int i = 0; i < maxUnitCount; i++) {
+                if ((i + 1) * splitUnit <= s.length()) {
+                    String unit = s.substring(splitUnit * i, splitUnit * (i + 1));
+                    units.push(unit);
+                }
+            }
+            String recentUnit = units.pop();
+            int tempAnswer = s.length();
+            int count = 1;
+            boolean isSame = false;
+
+            while (!units.isEmpty()) {
+                String nextUnit = units.pop();
+                if (nextUnit.equals(recentUnit)) {
+                    isSame = true;
+                    count++;
+                } else {
+                    if (isSame) {
+                        isSame = false;
+                        String countToString = Integer.toString(count);
+                        tempAnswer -= splitUnit * (count - 1) - countToString.length();
+                        count = 1;
+                    }
+                }
+                recentUnit = nextUnit;
+            }
+
+            if (isSame) {
+                String countToString = Integer.toString(count);
+                tempAnswer -= splitUnit * (count - 1) - countToString.length();
+            }
+
+            answer = Math.min(answer, tempAnswer);
+            splitUnit--;
+        }
+        return answer;
+    }
+}
