8a5d6e1ca3322d08611701e2f85108c4b64a9357
seungchul <seungchul0118@gmail.com>
2023-09-24 15:43:16 +0900

    programmers lv.3 등굣길

--- src/main/java/bootcamp/example/lv3/GoingSchool.java
+++ src/main/java/bootcamp/example/lv3/GoingSchool.java
@@ -16,32 +16,21 @@ public class GoingSchool {
 
     public static int solution(int m, int n, int[][] puddles) {
         int answer = 0;
-        Queue<int[]> queue = new LinkedList<>();
         int[][] map = new int[n][m];
-        boolean[][] visited = new boolean[n][m];
         map[0][0] = 1;
-        queue.add(new int[]{0, 0});
 
         for (int[] puddle : puddles) {
             map[puddle[1]-1][puddle[0]-1] = -1;
         }
 
-        while (!queue.isEmpty()) {
-            int[] now = queue.poll();
-            visited[now[1]][now[0]] = true;
-            if ((now[0]+1 < m) && map[now[1]][now[0]+1] != -1) {
-                map[now[1]][now[0]+1] = (map[now[1]][now[0]+1]+map[now[1]][now[0]])%1000000007;
-                if (!visited[now[1]][now[0]+1]){
-                    queue.add(new int[]{now[0]+1, now[1]});
-                    visited[now[1]][now[0]+1] = true;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (map[i][j] == -1) {
+                    map[i][j] = 0;
+                    continue;
                 }
-            }
-            if ((now[1]+1 < n) && map[now[1]+1][now[0]] != -1) {
-                map[now[1]+1][now[0]] = (map[now[1]+1][now[0]]+map[now[1]][now[0]])%1000000007;
-                if (!visited[now[1]+1][now[0]]){
-                    queue.add(new int[]{now[0], now[1]+1});
-                    visited[now[1]+1][now[0]] = true;
-                }
+                if (i != 0) map[i][j] += map[i - 1][j] % 1000000007;
+                if (j != 0) map[i][j] += map[i][j - 1] % 1000000007;
             }
         }
 
