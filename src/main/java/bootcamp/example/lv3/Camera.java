5d5d4595bfb1df1a0e4bc491b4a17c0986b7e927
seungchul <seungchul0118@gmail.com>
2023-10-04 14:03:36 +0900

    programmers lv.3 단속카메라

---
+++ src/main/java/bootcamp/example/lv3/Camera.java
@@ -0,0 +1,28 @@
+package bootcamp.example.lv3;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Camera {
+    public static void main(String[] args) {
+        int[][] routes = {{-20, -15}, {-14, -5}, {-18, -13}, {-5, -3}};
+
+        System.out.println(solution(routes));
+    }
+
+    // 프로그래머스 Lv.3 단속카메라
+
+    public static int solution(int[][] routes) {
+        int answer = 0;
+        Arrays.sort(routes, Comparator.comparingInt(o -> o[1]));
+        int lastCamera = -30001;
+        for (int[] route : routes) {
+            if (route[0] > lastCamera) {
+                lastCamera = route[1];
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
