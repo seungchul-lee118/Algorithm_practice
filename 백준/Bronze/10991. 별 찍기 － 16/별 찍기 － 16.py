49bdbaa08e1195f5f089e7c46e07f9df6bbb784b
seungchul <seungchul0118@gmail.com>
2024-01-22 16:32:20 +0900

    [Bronze III] Title: 별 찍기 - 16, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10991. 별 찍기 － 16/별 찍기 － 16.py
+++ 백준/Bronze/10991. 별 찍기 － 16/별 찍기 － 16.py
@@ -1,7 +1,4 @@
 n = int(input())
 
 for i in range(1, n + 1):
-    str = ' '* (n - i)
-    for j in range(1, 2*i):
-        str += '*' if j % 2 == 1 else ' '
-    print(str)
+    print(' ' * (n - i) + '* ' * i)
