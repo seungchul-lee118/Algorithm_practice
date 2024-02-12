e4d20ce2784d0f61ca16e34ac244408c2471fe48
seungchul <seungchul0118@gmail.com>
2024-02-12 22:16:46 +0900

    [Silver IV] Title: 카드2, Time: 264 ms, Memory: 49032 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 1; i <= N; i++) {
+            queue.offer(i);
+        }
+
+        while (queue.size() > 1) {
+            queue.poll();
+            queue.offer(queue.poll());
+        }
+        System.out.println(queue.poll());
+    }
+}
