cf55777d2a32aa55d292109309ba0f4a7e21de5d
seungchul <seungchul0118@gmail.com>
2024-02-29 16:03:11 +0900

    [Gold V] Title: 톱니바퀴, Time: 228 ms, Memory: 17944 KB -BaekjoonHub

---
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Main {
+	static int[][] magnet = new int[4][8];
+	static int[] idxs;
+	
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+
+        idxs = new int[4];
+        int result = 0;
+
+        for (int i = 0; i < 4; i++) {
+        	String s = sc.next();
+            for (int j = 0; j < 8; j++) {
+                magnet[i][j] = s.charAt(j) - '0';
+            }
+        }
+
+        int K = sc.nextInt();
+        for (int i = 0; i < K; i++) {
+            int[] directions = new int[4];
+            getDirect(sc.nextInt() - 1, sc.nextInt(), directions);
+            rotate(directions);
+        }
+
+        for (int i = 0; i < 4; i++) {
+            if (magnet[i][idxs[i]] == 1) {
+                result += (int) Math.pow(2, i);
+            }
+        }
+        System.out.println(result);
+	}
+	
+	static void getDirect(int idx, int rotateDirection, int[] directions) {
+		if (idx < 0 || idx >= 4) {
+			return ;
+		}
+		directions[idx] = rotateDirection;
+		if (idx > 0 && directions[idx-1] == 0 && magnet[idx][(idxs[idx] + 6) % 8] != magnet[idx-1][(idxs[idx-1] + 2) % 8]) {
+			getDirect(idx - 1, rotateDirection * -1, directions);
+		}
+		if (idx < 3 && directions[idx+1] == 0 && magnet[idx][(idxs[idx] + 2) % 8] != magnet[idx+1][(idxs[idx+1] + 6) % 8]) {
+			getDirect(idx + 1, rotateDirection * -1, directions);
+		}
+	}
+	
+	static void rotate(int[] directions) {
+		for (int i = 0; i < 4; i++) {
+			idxs[i] = (idxs[i] + directions[i] * (-1) + 8) % 8;
+		}
+	}
+}
