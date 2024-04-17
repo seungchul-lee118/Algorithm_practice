e9a2a41e3c3853f12146acc033b94ed9a98298a0
seungchul <seungchul0118@gmail.com>
2024-04-17 10:19:37 +0900

    [Gold V] Title: 동전 1, Time: 220 ms, Memory: 17816 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.java
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 		
 		int N = sc.nextInt();
 		int k = sc.nextInt();
 		
-		int[][] dp = new int[N][k+1];
+		int[] dp = new int[k+1];
 		int[] coins = new int[N];
 		
 		for (int i = 0; i < N; i++) {
@@ -16,20 +16,17 @@ public class Main {
 		
 		for (int i = 0; i <= k; i++) {
 			if (i % coins[0] == 0) {
-				dp[0][i] = 1;
+				dp[i] = 1;
 			}
 		}
 		
 		for (int n = 1; n < N; n++) {
 			for (int i = 0; i <= k; i++) {
 				if (i >= coins[n]) {
-					dp[n][i] = dp[n-1][i] + dp[n][i-coins[n]];
-				} else {
-					dp[n][i] = dp[n-1][i];
+					dp[i] = dp[i] + dp[i-coins[n]];
 				}
 			}
 		}
-		System.out.println(dp[N-1][k]);
-		
+		System.out.println(dp[k]);
 	}
 }
